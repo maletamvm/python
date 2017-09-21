e76722f3f992bace675596ab3f5e0fe30c8090fc
maletamvm <32159580+maletamvm@users.noreply.github.com>
2017-09-21 17:13:53 +0300

    Add files via upload

---
+++ lab2.1.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+print('Hi,{} {} {}'.format(sys.argv[1],sys.argv[2],sys.argv[3]))
---
+++ lab2.2.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+nam1,nam2,nam3=input('Введіть ваше імя та прізвище й по-батькові:').split(sep=' ')
+print('Привіт, {} {} {}'.format(nam1,nam2,nam3))
